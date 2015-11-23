35b39ee8cf395b5fa1d0cfa42ff530ceaa97b883
Morgan Tenney <morgan.tenney@gmail.com>
2015-11-22 20:50:43 -0800

    Bullets fire from turret. Increased bullet speed so you can't outrace bullets.

--- Assets/Scripts/Player_Fire.cs
+++ Assets/Scripts/Player_Fire.cs
@@ -5,6 +5,7 @@ using UnityEngine.Networking;
 public class Player_Fire : NetworkBehaviour {
 
 	public GameObject projectile;
+	[SerializeField] Transform turretTransform;
 
 	void Start() {
 
@@ -23,16 +24,16 @@ public class Player_Fire : NetworkBehaviour {
 
 	[Command]
 	void CmdSpawnBasicBullet(float x, float y, float vx, float vy) {
-		GameObject bullet = Instantiate(projectile, new Vector2(x + 1, y + 1), Quaternion.identity) as GameObject;
+		GameObject bullet = Instantiate(projectile, new Vector2(x, y), Quaternion.identity) as GameObject;
 		bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(vx, vy);
 		NetworkServer.Spawn (bullet);
 	}
 
 	[ClientCallback]
 	void ShootBullet() {
-		float xPos = this.transform.position.x;
-		float yPos = this.transform.position.y;
-		Vector3 dir = GetComponent<ShipControl> ().getDirection () * 10;
+		float xPos = turretTransform.position.x;//this.transform.position.x;
+		float yPos = turretTransform.position.y;//this.transform.position.y;
+		Vector3 dir = GetComponent<ShipControl> ().getDirection () * 20;
 		CmdSpawnBasicBullet (xPos, yPos, dir.x, dir.y);
 	}
 		
