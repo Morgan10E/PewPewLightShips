e8281b37d392a46df70ea029c4fcc7ab22dde0e4
Thaminda Edirisooriya <tediris@stanford.edu>
2015-11-24 11:56:59 -0700

    made the trail transparent, fine tuning to after image

--- Assets/Scripts/AfterImage.cs
+++ Assets/Scripts/AfterImage.cs
@@ -1,18 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class AfterImage : MonoBehaviour {
+public class AfterImage : NetworkBehaviour {
 	public GameObject particlePrefab;
 	GameObject afterImage;
 	ParticleSystem pSys;
 
 	// Use this for initialization
 	void Start () {
+		// TODO: have this happen when the player connects
 		AttachParticleSystem ();
 	}
 
 	void AttachParticleSystem() {
-		GameObject afterImage = Instantiate(particlePrefab);
+		afterImage = Instantiate(particlePrefab);
 		afterImage.transform.parent = gameObject.transform;
 		pSys = afterImage.GetComponent<ParticleSystem> ();
 		pSys.enableEmission = false;
@@ -20,10 +22,42 @@ public class AfterImage : MonoBehaviour {
 
 	public void EnableAfterImage() {
 		pSys.startRotation = ((gameObject.transform.eulerAngles.z - 180) % 360) * Mathf.Deg2Rad;
+		//afterImage.transform.parent = gameObject.transform;
+		
 		pSys.enableEmission = true;
+
+		if (isLocalPlayer) {
+			SetAfterImage(true);
+		}
 	}
 
 	public void DisableAfterImage() {
 		pSys.enableEmission = false;
+		if (isLocalPlayer) {
+			SetAfterImage(false);
+		}
+	}
+
+	[ClientRpc]
+	void RpcSetAfterImageClient(bool enable) {
+		if (isLocalPlayer)
+			return;
+		if (enable) {
+			pSys.startRotation = ((gameObject.transform.eulerAngles.z - 180) % 360) * Mathf.Deg2Rad;
+			//afterImage.transform.parent = gameObject.transform;
+			pSys.enableEmission = true;
+		} else {
+			pSys.enableEmission = false;
+		}
+	}
+
+	[Command]
+	void CmdEnableAfterImage(bool enable) {
+		RpcSetAfterImageClient (enable);
+	}
+
+	[ClientCallback]
+	void SetAfterImage(bool enable) {
+		CmdEnableAfterImage (enable);
 	}
 }
--- Assets/Scripts/Player_SyncPosition.cs
+++ Assets/Scripts/Player_SyncPosition.cs
@@ -41,7 +41,13 @@ public class Player_SyncPosition : NetworkBehaviour {
 	}
 
 	void OrdinaryLerp() {
-		myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime * LerpRate);
+		Vector2 deltaVec = myTransform.position - syncPos;
+		float distanceDelta = Mathf.Abs(deltaVec.x) + Mathf.Abs (deltaVec.y);
+		if (distanceDelta > 0.1) {
+			myTransform.position = Vector3.Lerp (myTransform.position, syncPos, Time.deltaTime * LerpRate);
+		} else {
+			myTransform.position = syncPos;
+		}
 		turretTransform.position = Vector3.Lerp (turretTransform.position, syncTurretPos, Time.deltaTime * LerpRate);
 	}
 
